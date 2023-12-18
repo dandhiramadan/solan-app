f7a0400589ec1445a0288c5fada754d7984cc233
dandhiramadan <dandhiramadan122098@gmail.com>
2023-12-18 23:01:40 +0700

    login start

---
+++ app/Livewire/Login/LoginPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Login;
+
+use Livewire\Component;
+
+class LoginPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.login.login-page');
+    }
+}
