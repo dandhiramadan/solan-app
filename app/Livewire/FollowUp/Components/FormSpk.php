d54c575c6cb6cc50c03a4ec80c09da9971cb35ce
dandhiramadan <dandhiramadan122098@gmail.com>
2023-12-18 22:06:08 +0700

    integrasi template

---
+++ app/Livewire/FollowUp/Components/FormSpk.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\FollowUp\Components;
+
+use Livewire\Component;
+
+class FormSpk extends Component
+{
+    public function render()
+    {
+        return view('livewire.follow-up.components.form-spk');
+    }
+}
