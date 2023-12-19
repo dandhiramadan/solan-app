7059f004712debb3cf8969e963468787cac8a0bc
dandhiramadan <dandhiramadan122098@gmail.com>
2023-12-20 01:34:59 +0700

    form spk

--- resources/views/livewire/follow-up/components/form-spk.blade.php
+++ resources/views/livewire/follow-up/components/form-spk.blade.php
@@ -1,4 +1,72 @@
 <div>
     {{-- Stop trying to control. --}}
-    as
+    <h4 class="py-3 mb-4"><span class="text-muted fw-light">Form SPK /</span> New SPK</h4>
+    <!-- Basic Layout -->
+    <div class="card mb-4">
+        <h5 class="card-header">Form New SPK</h5>
+        <form class="card-body">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="row">
+                        <label class="form-label" for="collapsible-fullname">Jenis Spk</label>
+                        <div class="col-md">
+                            <div class="form-check custom-option custom-option-basic">
+                                <label class="form-check-label custom-option-content" for="customRadioTemp1">
+                                    <input name="customRadioTemp" class="form-check-input" type="radio" value=""
+                                        id="customRadioTemp1" checked />
+                                    <span class="custom-option-header">
+                                        <span class="h6 mb-0">Layout</span>
+                                    </span>
+                                </label>
+                            </div>
+                        </div>
+                        <div class="col-md">
+                            <div class="form-check custom-option custom-option-basic">
+                                <label class="form-check-label custom-option-content" for="customRadioTemp2">
+                                    <input name="customRadioTemp" class="form-check-input" type="radio" value=""
+                                        id="customRadioTemp2" />
+                                    <span class="custom-option-header">
+                                        <span class="h6 mb-0">Sample</span>
+                                    </span>
+                                </label>
+                            </div>
+                        </div>
+                        <div class="col-md">
+                            <div class="form-check custom-option custom-option-basic">
+                                <label class="form-check-label custom-option-content" for="customRadioTemp3">
+                                    <input name="customRadioTemp" class="form-check-input" type="radio" value=""
+                                        id="customRadioTemp3" />
+                                    <span class="custom-option-header">
+                                        <span class="h6 mb-0">Production</span>
+                                    </span>
+                                </label>
+                            </div>
+                        </div>
+                        <div class="col-md">
+                            <div class="form-check custom-option custom-option-basic">
+                                <label class="form-check-label custom-option-content" for="customRadioTemp4">
+                                    <input name="customRadioTemp" class="form-check-input" type="radio" value=""
+                                        id="customRadioTemp4" />
+                                    <span class="custom-option-header">
+                                        <span class="h6 mb-0">Stock</span>
+                                    </span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <label class="form-label" for="defaultInput">Full Name</label>
+                    <input
+                      type="text"
+                      id="defaultInput"
+                      class="form-control"
+                      placeholder="John Doe" />
+                </div>
+            </div>
+            <div class="pt-4">
+                <button type="submit" class="btn btn-primary me-sm-3 me-1">Submit</button>
+            </div>
+        </form>
+    </div>
 </div>
