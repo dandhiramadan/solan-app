25a9670c9100db94a1a242c3d6b73285495d8c6b
dandhiramadan <dandhiramadan122098@gmail.com>
2023-12-26 15:02:44 +0700

    logo login

---
+++ resources/views/components/partials/footer.blade.php
@@ -0,0 +1,13 @@
+<!-- Footer -->
+<footer class="content-footer footer bg-footer-theme">
+    <div class="container-xxl">
+        <div
+            class="footer-container d-flex align-items-center justify-content-between py-2 flex-md-row flex-column">
+            <div>
+                © 2023
+                , made with ❤️ by Domdom</a>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- / Footer -->
