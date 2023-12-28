9f957f06cff8f95d7aa0078efdfa4c6d0a06ed75
dandhiramadan <dandhiramadan122098@gmail.com>
2023-12-29 01:08:17 +0700

    autocomplete

--- resources/views/livewire/auth/login-page.blade.php
+++ resources/views/livewire/auth/login-page.blade.php
@@ -2,100 +2,87 @@
     {{-- Care about people's approval and you will be their prisoner. --}}
     <div class="authentication-wrapper authentication-cover authentication-bg">
         <div class="authentication-inner row">
-          <!-- /Left Text -->
-          <div class="d-none d-lg-flex col-lg-7 p-0">
-            <div class="auth-cover-bg auth-cover-bg-color d-flex justify-content-center align-items-center">
-              <img
-                src="assets/img/illustrations/auth-login-illustration-light.png"
-                alt="auth-login-cover"
-                class="img-fluid my-5 auth-illustration"
-                data-app-light-img="illustrations/auth-login-illustration-light.png"
-                data-app-dark-img="illustrations/auth-login-illustration-dark.png" />
+            <!-- /Left Text -->
+            <div class="d-none d-lg-flex col-lg-7 p-0">
+                <div class="auth-cover-bg auth-cover-bg-color d-flex justify-content-center align-items-center">
+                    <img src="assets/img/illustrations/auth-login-illustration-light.png" alt="auth-login-cover"
+                        class="img-fluid my-5 auth-illustration"
+                        data-app-light-img="illustrations/auth-login-illustration-light.png"
+                        data-app-dark-img="illustrations/auth-login-illustration-dark.png" />
 
-              <img
-                src="assets/img/illustrations/bg-shape-image-light.png"
-                alt="auth-login-cover"
-                class="platform-bg"
-                data-app-light-img="illustrations/bg-shape-image-light.png"
-                data-app-dark-img="illustrations/bg-shape-image-dark.png" />
+                    <img src="assets/img/illustrations/bg-shape-image-light.png" alt="auth-login-cover"
+                        class="platform-bg" data-app-light-img="illustrations/bg-shape-image-light.png"
+                        data-app-dark-img="illustrations/bg-shape-image-dark.png" />
+                </div>
             </div>
-          </div>
-          <!-- /Left Text -->
+            <!-- /Left Text -->
 
-          <!-- Login -->
-          <div class="d-flex col-12 col-lg-5 align-items-center p-sm-5 p-4">
-            <div class="w-px-400 mx-auto">
-              <!-- Logo -->
-              <div class="app-brand mb-4">
-                <a href="/" class="app-brand-link gap-2">
-                    <span class="app-brand-logo h-px-32 w-px-32">
-                        <!-- Add image -->
-                        <img src="{{ asset('assets/img/branding/logo.png') }}" alt="logo">
-                        <!--/ Add image -->
-                    </span>
-                </a>
-              </div>
-              <!-- /Logo -->
-              <h3 class="mb-1">Welcome to Solan Apps! 👋</h3>
-              <p class="mb-4">Please sign-in to your account and start the adventure</p>
+            <!-- Login -->
+            <div class="d-flex col-12 col-lg-5 align-items-center p-sm-5 p-4">
+                <div class="w-px-400 mx-auto">
+                    <!-- Logo -->
+                    <div class="app-brand mb-4">
+                        <a href="/" class="app-brand-link gap-2">
+                            <span class="app-brand-logo h-px-32 w-px-32">
+                                <!-- Add image -->
+                                <img src="{{ asset('assets/img/branding/logo.png') }}" alt="logo">
+                                <!--/ Add image -->
+                            </span>
+                        </a>
+                    </div>
+                    <!-- /Logo -->
+                    <h3 class="mb-1">Welcome to Solan Apps! 👋</h3>
+                    <p class="mb-4">Please sign-in to your account and start the adventure</p>
 
-              @if (session()->has('error'))
-              <div class="alert alert-primary d-flex align-items-center" role="alert">
-                <span class="alert-icon text-primary me-2">
-                  <i class="ti ti-x ti-xs"></i>
-                </span>
-                {{ session('error') }}
-              </div>
-              @endif
+                    @if (session()->has('error'))
+                        <div class="alert alert-primary d-flex align-items-center" role="alert">
+                            <span class="alert-icon text-primary me-2">
+                                <i class="ti ti-x ti-xs"></i>
+                            </span>
+                            {{ session('error') }}
+                        </div>
+                    @endif
 
-              <form id="formAuthentication" class="mb-3"  wire:submit="login">
-                <div class="mb-3">
-                  <label for="username" class="form-label">Username</label>
-                  <input
-                    type="text"
-                    class="form-control @error('username') is-invalid @enderror"
-                    id="username"
-                    name="username"
-                    wire:model.defer="username"
-                    placeholder="Enter your username"
-                    autofocus />
+                    <form id="formAuthentication" class="mb-3" wire:submit="login">
+                        <div class="mb-3">
+                            <label for="username" class="form-label">Username</label>
+                            <input type="text" class="form-control @error('username') is-invalid @enderror"
+                                id="username" name="username" wire:model.defer="username"
+                                placeholder="Enter your username" autocomplete="off" autofocus />
 
-                    @error('username')
-                        <div class="invalid-feedback">Please enter your name.</div>
-                    @enderror
+                            @error('username')
+                                <div class="invalid-feedback">Please enter your name.</div>
+                            @enderror
 
+                        </div>
+                        <div class="mb-3 form-password-toggle">
+                            <div class="d-flex justify-content-between">
+                                <label class="form-label" for="password">Password</label>
+                            </div>
+                            <div class="input-group input-group-merge">
+                                <input type="password" id="password"
+                                    class="form-control @error('password') is-invalid @enderror" name="password"
+                                    wire:model.defer="password"
+                                    placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                                    aria-describedby="password" />
+
+                                <span class="input-group-text cursor-pointer"><i class="ti ti-eye-off"></i></span>
+                                @error('password')
+                                    <div class="invalid-feedback">Please enter your password.</div>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="remember-me" />
+                                <label class="form-check-label" for="remember-me"> Remember Me </label>
+                            </div>
+                        </div>
+                        <button class="btn btn-primary d-grid w-100" type="submit">Sign in</button>
+                    </form>
                 </div>
-                <div class="mb-3 form-password-toggle">
-                  <div class="d-flex justify-content-between">
-                    <label class="form-label" for="password">Password</label>
-                  </div>
-                  <div class="input-group input-group-merge">
-                    <input
-                      type="password"
-                      id="password"
-                      class="form-control @error('password') is-invalid @enderror"
-                      name="password"
-                      wire:model.defer="password"
-                      placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
-                      aria-describedby="password" />
-
-                    <span class="input-group-text cursor-pointer"><i class="ti ti-eye-off"></i></span>
-                    @error('password')
-                        <div class="invalid-feedback">Please enter your password.</div>
-                    @enderror
-                  </div>
-                </div>
-                <div class="mb-3">
-                  <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="remember-me" />
-                    <label class="form-check-label" for="remember-me"> Remember Me </label>
-                  </div>
-                </div>
-                <button class="btn btn-primary d-grid w-100" type="submit">Sign in</button>
-              </form>
             </div>
-          </div>
-          <!-- /Login -->
+            <!-- /Login -->
         </div>
-      </div>
+    </div>
 </div>
